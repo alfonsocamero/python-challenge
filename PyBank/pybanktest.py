9502c823387005465798f739e50605ff484a4f7c
Alfonso Camero <alfonsocamero@MacBook-Pro.attlocal.net>
2021-06-28 15:25:30 -0400

    Started over. Created a list storing all profit/loss values

--- PyBank/pybanktest.py
+++ PyBank/pybanktest.py
@@ -17,4 +17,12 @@ with open(csvpath) as csvfile:
 
     for row in budget_data:
 	    profit_loss.append(row[1])
-    print(profit_loss)
+    #print(profit_loss)
+
+for net in range(len(profit_loss)):
+    total_months = len(profit_loss)
+    net_total = net_total +(int(profit_loss[net]))
+    avg_delta.append((int(profit_loss[net])) - (int(profit_loss[net-1])))
+print(total_months)
+print(net_total)
+print(avg_delta[net - 1])
