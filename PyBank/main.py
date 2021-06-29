13bf63142a8432566edeb349711b82468411befd
Alfonso Camero <alfonsocamero@MacBook-Pro.attlocal.net>
2021-06-28 20:01:59 -0400

    calculated greatest increase and greatest decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,7 @@ import csv
 
 total_months = 0
 profit_loss = []
+dates_list = []
 net_total = 0
 running_delta = []
 running_delta_total = 0
@@ -16,8 +17,9 @@ with open(csvpath) as csvfile:
 
     csv_header = next(budget_data)
 
-    for row in budget_data:
-	    profit_loss.append(row[1])
+    for col in budget_data:
+        profit_loss.append(col[1])
+        dates_list.append(col[0])
     
 for net in range(len(profit_loss)):
     total_months = len(profit_loss)
@@ -27,8 +29,9 @@ for net in range(len(profit_loss)-1):
     running_delta.append((int(profit_loss[net+1])) - (int(profit_loss[net])))
     running_delta_total = running_delta_total +(int(running_delta[net]))
     avg_delta = running_delta_total/(len(running_delta))
-print("Total months: " + total_months)
-print("Total: " + net_total)
-print(profit_loss)
-print(running_delta_total)
-print("Average Change: " + avg_delta)
+    greatest_increase = max(running_delta)
+    greatest_decrease = min(running_delta)
+
+print("Total months: " + str(total_months))
+print("Total: " + str(net_total))
+print("Average Change: " + str(avg_delta))
